58cb6ca47b332a4d44668a0611f4008231d14d3e
NicolasArroyo <nicolas.arroyo.c@utec.edu.pe>
2024-07-18 13:28:43 -0500

    refactor of main

--- main.cpp
+++ main.cpp
@@ -8,180 +8,143 @@
 
 using namespace std;
 
-void testHashTables() {
-    ll L = 4;
-    ll T = 50;
-    ld w = 5;
-
-    HashTables hashTable(L, T, w, 3);
-
-    vector<vector<ld>> vectors = {
-            {1.0,   2.0,   3.0},
-            {1.1,   2.1,   3.1},
-            {10.0,  20.0,  30.0},
-            {10.1,  20.1,  30.1},
-            {100.0, 200.0, 300.0},
-            {100.1, 200.1, 300.1}
-    };
-
-    for (const auto &vec: vectors) {
-        hashTable.insert(vec);
-    }
-
-    vector<ld> query = {1.05, 2.05, 3.05};
-    unordered_set<vector<ld>, VectorHash<ld>, VectorEqual<ld>> results = hashTable.search(query);
-
-    cout << "Nearby vectors:" << endl;
-
-    for (const auto &res: results) {
-        cout << "[";
-
-        for (size_t i = 0; i < res.size(); ++i) {
-            cout << res[i];
-            if (i < res.size() - 1) cout << ", ";
-        }
-        cout << "]" << endl;
-    }
-
-}
-
 vector<ld> generatePointInRange(ld minVal, ld maxVal) {
-  random_device rd;
-  mt19937 gen(rd());
-  uniform_real_distribution<ld> dis(minVal, maxVal);
-  return {dis(gen), dis(gen), dis(gen)};
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<ld> dis(minVal, maxVal);
+
+    return {dis(gen), dis(gen), dis(gen)};
 }
 
 vector<ld> generatePointInTwoRanges(ld minVal1, ld maxVal1, ld minVal2, ld maxVal2) {
-  random_device rd;
-  mt19937 gen(rd());
-  uniform_real_distribution<ld> dis(0.0, 1.0);
-  vector<ld> point(3);
-  for (int i = 0; i < 3; ++i) {
-    if (dis(gen) < 0.5) {
-      point[i] = generatePointInRange(minVal1, maxVal1)[i];
-    } 
-    else {
-      point[i] = generatePointInRange(minVal2, maxVal2)[i];
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<ld> dis(0.0, 1.0);
+
+    vector<ld> point(3);
+    for (int i = 0; i < 3; ++i) {
+        if (dis(gen) < 0.5) {
+            point[i] = generatePointInRange(minVal1, maxVal1)[i];
+        }
+        else {
+            point[i] = generatePointInRange(minVal2, maxVal2)[i];
+        }
     }
-  }
-  return point;
+
+    return point;
 }
 
-void writePointsToFile(const vector<vector<ld>>& data, const string& filename) {
-  ofstream outputFile(filename);
-  if (outputFile.is_open()) {
-    for (const auto& point : data) {
-      outputFile << point[0] << ", " << point[1] << ", " << point[2] << endl;
+void writePointsToFile(const vector<vector<ld>> &data, const string &filename) {
+    ofstream outputFile(filename);
+
+    if (outputFile.is_open()) {
+        for (const auto &point: data) {
+            outputFile << point[0] << ", " << point[1] << ", " << point[2] << endl;
+        }
+        outputFile.close();
     }
-    outputFile.close();
-  }
 }
 
 void testLSHATrain(LSHAD &lshad) {
-  int numPoints = 99;
-  int numClosePoints = numPoints * 0.9;
-  int numFarPoints = numPoints - numClosePoints;
-  vector<vector<ld>> data;
-  vector<vector<ld>> closePoints;
-  for (int i = 0; i < numClosePoints; ++i) {
-    closePoints.push_back(generatePointInRange(-10.0, 10.0));
-  }
-  vector<vector<ld>> farPoints;
-  for (int i = 0; i < numFarPoints; ++i) {
-    farPoints.push_back(generatePointInTwoRanges(-100.0, -50.0, 50.0, 100.0));
-  }
+    /////////////////////////////////////////////////////////////////////////
+    // RANDOM POINTS CREATION ///////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
 
-  data.insert(data.end(), closePoints.begin(), closePoints.end());
-  vector<ld> query1 = data[0];
-  // data.push_back({50, 50, 50});
-  data.insert(data.end(), farPoints.begin(), farPoints.end());
+    int numPoints = 100;
 
-  random_device rd;
-  mt19937 g(rd());
-  shuffle(data.begin(), data.end(), g);
+    int numNonAnomalyPoints = int(numPoints * 0.9);
+    int numAnomalyPoints = numPoints - numNonAnomalyPoints;
 
-  writePointsToFile(data, "points.txt");
-  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-  lshad.train(data, (ld) 0.1);
+    vector<vector<ld>> data;
 
-  vector<ld> query2 = {50, 50, 50};
+    vector<vector<ld>> closePoints;
+    closePoints.reserve(numNonAnomalyPoints);
+    for (int i = 0; i < numNonAnomalyPoints; ++i) {
+        closePoints.push_back(generatePointInRange(-10.0, 10.0));
+    }
+    data.insert(data.end(), closePoints.begin(), closePoints.end());
 
-  ll count_close = 0;
-  cout << "\nclose points: " << endl;
-  for(auto point: closePoints) {
-    bool isAnomaly = lshad.detection_phase(point);
-    cout << isAnomaly << endl;
-    count_close = !isAnomaly ? count_close + 1 : count_close;
-  }
+    vector<vector<ld>> farPoints;
+    farPoints.reserve(numAnomalyPoints);
+    for (int i = 0; i < numAnomalyPoints; ++i) {
+        farPoints.push_back(generatePointInTwoRanges(-100.0, -50.0, 50.0, 100.0));
+    }
+    data.insert(data.end(), farPoints.begin(), farPoints.end());
 
-  ll count_far = 0;
-  cout << "\nfar points: " << endl;
-  for(auto point: farPoints) {
-    bool isAnomaly = lshad.detection_phase(point);
-    cout << isAnomaly << endl;
-    count_far = isAnomaly ? count_far + 1 : count_far;
-  }
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(data.begin(), data.end(), g);
+    writePointsToFile(data, "points.txt");
 
-  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-  cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+    /////////////////////////////////////////////////////////////////////////
+    // TRAINING PHASE ///////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
 
-  cout << "Not anomaly accuracy: " << (ld) count_close / numClosePoints << endl;
-  cout << "Anomaly accuracy: " << (ld) count_far / numFarPoints << endl;
-}
+    chrono::steady_clock::time_point training_start_clock = chrono::steady_clock::now();
 
-void testLSHADHyperparametersAutotuning() {
-    vector<vector<ld>> data = {
-            {1.0,   2.0,   3.0},
-            {1.1,   2.1,   3.1},
-            {10.0,  20.0,  30.0},
-            {10.1,  20.1,  30.1},
-            {100.0, 200.0, 300.0},
-            {100.1, 200.1, 300.1},
-            {100.0, 200.0, 303.0},
-            {100.1, 210.1, 300.1},
-            {50.0, 50.0, 40.0},
-            {45.1, 45.1, 45.1},
-            {45.0, 45.0, 45.0},
-            {100000.1, 200000.1, 300000.1}
-    };
+    lshad.train(data, 0.1);
 
-    LSHAD lshad;
-    tuple<ll, ll, ld> hyperparameters = lshad.tuneHyperparameters(data);
+    chrono::steady_clock::time_point training_end_clock = chrono::steady_clock::now();
+    cout << "Training phase elapsed time: "
+         << chrono::duration_cast<chrono::milliseconds>(training_end_clock - training_start_clock).count()
+         << "[ms]"
+         << endl;
 
-    cout << "Hyperparameters: " << endl;
-    cout << "L: " << get<0>(hyperparameters) << endl;
-    cout << "T: " << get<1>(hyperparameters) << endl;
-    cout << "w: " << get<2>(hyperparameters) << endl;
-}
+    /////////////////////////////////////////////////////////////////////////
+    // QUERIES PHASE  ///////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
 
-void testEstPerHash() {
-  vector<vector<ld>> data = {
-    {1.0,   2.0,   3.0},
-    {1.1,   2.1,   3.1},
-    {1.0,  2.0,  3.1},
-    {10.1,  20.1,  30.1},
-    {15.0, 25.0, 26.0},
-    {15.1, 25.1, 27.0},
-    {50.0, 40.0, 30.0},
-    {9.1, 7.1, 10.1},
-    {16.0, 15.0, 27.0},
-    {21.1, 12.1, 35.5},
-    {100000.0, 200000.0, 300000.0},
-    {100000.1, 200000.1, 300000.1}
-  };
-  LSHAD lshad;
-  lshad.train(data, (ld) 0.1);
+    chrono::steady_clock::time_point query_start_clock = chrono::steady_clock::now();
+
+    ll nonAnomalyCounter = 0;
+    cout << "\nNon-anomaly points: " << endl;
+    for (const auto &point: closePoints) {
+        bool isAnomaly = lshad.detection_phase(point);
+        nonAnomalyCounter = !isAnomaly ? nonAnomalyCounter + 1 : nonAnomalyCounter;
+
+        cout << "("
+             << point[0] << ", "
+             << point[1] << ", "
+             << point[2] << ")"
+             << " = " << boolalpha << isAnomaly
+             << endl;
+    }
+
+    ll anomalyCounter = 0;
+    cout << "\nAnomaly points: " << endl;
+    for (const auto &point: farPoints) {
+        bool isAnomaly = lshad.detection_phase(point);
+        anomalyCounter = isAnomaly ? anomalyCounter + 1 : anomalyCounter;
+
+        cout << "("
+             << point[0] << ", "
+             << point[1] << ", "
+             << point[2] << ")"
+             << " = " << boolalpha << isAnomaly
+             << endl;
+    }
+
+    cout << "\nNot-anomaly accuracy: "
+         << (ld) nonAnomalyCounter / numNonAnomalyPoints
+         << endl;
+
+    cout << "Anomaly accuracy: "
+         << (ld) anomalyCounter / numAnomalyPoints
+         << endl;
+
+    chrono::steady_clock::time_point query_end_clock = chrono::steady_clock::now();
+
+    cout << "Queries phase elapsed time: "
+         << chrono::duration_cast<chrono::milliseconds>(query_end_clock - query_start_clock).count()
+         << "[ms]"
+         << endl;
+
+    /////////////////////////////////////////////////////////////////////////
+    // QUERIES PHASE END ////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
 }
 
 int main() {
-  // testHashTables();
-  // testLSHADHyperparametersAutotuning();
-  // testEstPerHash();
-  LSHAD lshad;
-
-  testLSHATrain(lshad);
-  // testEstPerHash();
-
-  return 0;
+    LSHAD lshad;
+    testLSHATrain(lshad);
 }
